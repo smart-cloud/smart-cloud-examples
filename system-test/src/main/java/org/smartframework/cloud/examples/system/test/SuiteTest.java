2a1aa73fc305fc0b483625f86843526ccf6e996d
luckyQing <1634753825@qq.com>
2019-12-05 20:44:59 +0800

    refactor: move class

--- system-test/src/main/java/org/smartframework/cloud/examples/system/test/SuiteTest.java
+++
@@ -1,9 +0,0 @@
-package org.smartframework.cloud.examples.system.test;
-
-import org.junit.runner.RunWith;
-import org.smartframework.cloud.starter.test.runner.AllTestsRunner;
-
-@RunWith(AllTestsRunner.class)
-public class SuiteTest {
-
-}
