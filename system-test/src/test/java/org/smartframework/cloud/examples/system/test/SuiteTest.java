c181e5578d26e9c29ab6a315c15cb2ea3333a493
luckyQing <1634753825@qq.com>
2021-10-17 20:05:32 +0800

    refactor: exclude suite test case when build by mvn

--- system-test/src/test/java/org/smartframework/cloud/examples/system/test/SuiteTest.java
+++ system-test/src/test/java/org/smartframework/cloud/examples/system/test/SuiteTest.java
@@ -1,9 +1,11 @@
 package org.smartframework.cloud.examples.system.test;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
+@Disabled
 @RunWith(JUnitPlatform.class)
 @SelectPackages({"org.smartframework.cloud.examples.system.test"})
 public class SuiteTest {
