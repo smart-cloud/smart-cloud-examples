da0006fbd6d3ecfba42170b98fae3108a8c2c5df
luckyQing <1634753825@qq.com>
2024-02-01 16:04:13 +0800

    feature: 监控集成monitor模块

--- application-module/support-module/support-service-admin/src/main/java/org/smartframework/cloud/examples/support/admin/AdminApplication.java
+++ application-module/support-module/support-service-admin/src/main/java/org/smartframework/cloud/examples/support/admin/AdminApplication.java
@@ -19,15 +19,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-
-@SpringBootApplication
-@EnableAdminServer
 @EnableDiscoveryClient
+@SpringBootApplication
 public class AdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
 
 }
--- application-module/support-module/support-service-admin/src/main/java/org/smartframework/cloud/examples/support/admin/service/IRobotService.java
+++
@@ -1,9 +0,0 @@
-package org.smartframework.cloud.examples.support.admin.service;
-
-import org.smartframework.cloud.examples.support.admin.enums.RobotTemplateCode;
-
-public interface IRobotService {
-
-    void sendWxworkNotice(RobotTemplateCode robotTemplateCode, String name, String address, String state, String healthInstanceCountDesc);
-
-}
